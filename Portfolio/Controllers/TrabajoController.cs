af6bd0c765b0a6558cb54d3a5bbc5e0d95920c3b
Emanuel Infante <pei2688@gmail.com>
2025-04-23 22:06:20 -0300

    Se agregan funcionalidades para subir los trabajos  y poder mostrarlos

---
+++ Portfolio/Controllers/TrabajoController.cs
@@ -0,0 +1,53 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Collections.Generic;
+
+namespace Portfolio.Controllers
+{
+    public class TrabajoController:Controller
+    {
+        
+        private static List<Trabajo> trabajo = new List<Trabajo>();
+        
+        [HttpGet]
+        public IActionResult SubirTrabajo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubirTrabajo(string Titulo, string Descripcion, DateTime Fecha, IFormFile Imagen)
+        {
+            if (Imagen != null && Imagen.Length > 0)
+            {
+                var nombreArchivo = Guid.NewGuid().ToString() + Path.GetExtension(Imagen.FileName);
+                var rutaGuardar = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "imagenes", nombreArchivo);
+
+                using (var stream = new FileStream(rutaGuardar, FileMode.Create))
+                {
+                    Imagen.CopyTo(stream);
+                }
+
+                var nuevoTrabajo = new Trabajo
+                {
+                    Id = trabajo.Count + 1,
+                    Titulo = Titulo,
+                    Descripcion = Descripcion,
+                    Fecha = Fecha,
+                    ImagenUrl = "/imagenes/" + nombreArchivo
+                };
+                trabajo.Add(nuevoTrabajo);
+
+                return RedirectToAction("Galeria");
+            }
+        
+            ViewBag.Mensaje = "La imagen no es valida";
+            return View();
+        }
+        public IActionResult Galeria()
+        {
+            return View(trabajo);
+        }
+    }
+}
---
+++ Portfolio/Models/Trabajo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Portfolio.Models
+{
+    public class Trabajo
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
